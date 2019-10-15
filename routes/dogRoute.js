c467e7b5ba15392becacc7ad362944178c4616b8
Lynn Knapp <ebonyeek@gmail.com>
2019-10-15 14:59:51 -0600

    finished server

---
+++ routes/dogRoute.js
@@ -0,0 +1,65 @@
+const express =require('express')
+const dogRouter =express.Router()
+const Dog= require('../models/dogSchema.js')
+
+dogRouter.get('/', (req, res, next) =>{
+    Dog.find((err, dogs) =>{
+        if(err) {
+            res.status(500)
+            return next
+        }
+        return res.status(200).send(dogs)
+    })
+})
+
+dogRouter.get('/:_id', (req, res, next)=>{
+    Dog.findOne({_id: req.params._id}, (err, dog) =>{
+        if(err){
+            res.status(500)
+            return next(err)
+        }else if(!dog){
+            res.status(404)
+            return next(new Error('dog not found'))
+        }
+        return res.status(201).send(dog)
+
+    })
+})
+
+dogRouter.post('/', (req, res, next)=>{
+    const newDog = new Dog(req.body)
+    newDog.save((err, newDog)=>{
+        if(err) {
+            res.status(500)
+            return next(err)
+        }
+    })
+    return res.status(200).send(newDog)
+})
+
+dogRouter.put('/',(req,res,next)=>[
+    Dog.findOneAndUpdate(
+        {_id: req.params._id},
+        req.body,
+        {new: true},
+        (err,dog) =>{
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(dog)    
+        })    
+])
+dogRouter.delete('/:_id', (req, res, next) =>{
+    Dog.findOneByIdAndDelete(req.params._id, (err, dog) =>{
+        if(err){
+            res.status(500)
+        }
+        return res.status(200).send('sucessfully deleted ')
+    })
+})
+
+
+
+
+moduke.exports = dogRoute
