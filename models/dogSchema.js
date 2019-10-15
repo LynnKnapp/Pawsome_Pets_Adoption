b5aca8a3c1627c8d6025034a3b382203641d887e
Randon Shaw <randon.shaw@gmail.com>
2019-10-15 14:55:18 -0600

    dog and cat schema configured

---
+++ models/dogSchema.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const dogSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    breed: {
+        type: String,
+        default: "unknown"
+    },
+    size: {
+        type: String,
+        enum: ["small", "medium", "large", "extra large"]
+    },
+    age: {
+        type: Number,
+        default: "unknown"
+    },
+    shed: {
+        type: Boolean,
+        default: true
+    },
+    goodWithKids: {
+        type: Boolean,
+        default: true
+    },
+    goodWithPets: {
+        type: Boolean,
+        default: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    furColor: {
+        type: String,
+        default: "unknown"
+    }
+})
+
+module.exports = mongoose.model("Dog", dogSchema)
