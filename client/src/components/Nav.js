8a2dc327a1769cf08897582970aaf51143490a44
Caetlyn Venche <caetlynvenche@gmail.com>
2019-10-16 18:11:54 -0600

    About general layout and spacing

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
     return (
         <nav>
+            <h1>PAWSOME PETS</h1>
             <Link to="/">About</Link>
             <Link to="/available">Available</Link>
             <Link to="/adopt">Adopt</Link>
