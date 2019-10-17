0e1b9777513d3d289d5b7435c04dcf5222301714
Caetlyn Venche <caetlynvenche@gmail.com>
2019-10-17 11:54:11 -0600

    adopt framework

---
+++ client/src/components/Home.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="homeMain">
+            <section className="whoWeAre">
+                <h2>Who We Are</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde assumenda at tempora, asperiores fugit perspiciatis officiis soluta, nisi ducimus et hic ad, enim sequi ut harum est repudiandae. Iure, ea.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aperiam ducimus esse odio animi voluptate dignissimos dicta sint dolore ipsam eveniet officiis, eaque, cupiditate obcaecati deleniti modi, reprehenderit consequuntur vero.</p>
+            </section>
+
+            
+
+            <div className="images">
+                <p>dog carosel</p>
+            </div>
+
+            <div className="images">
+                <p>cat carosel</p>
+            </div>
+            
+            <div className="testimony">
+                <section>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odio reiciendis et magnam consequuntur quo.</p>
+                    <h3>-Name</h3>
+                </section>
+                <section></section>
+                <section></section>
+                <section>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odio reiciendis et magnam consequuntur quo.</p>
+                    <h3>-Name</h3>
+                </section>
+                <section>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odio reiciendis et magnam consequuntur quo.</p>
+                    <h3>-Name</h3>
+                </section>
+                <section></section>
+                <section></section>
+                <section>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odio reiciendis et magnam consequuntur quo.</p>
+                    <h3>-Name</h3>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Home
