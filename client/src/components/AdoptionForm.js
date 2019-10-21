8aea32d32898768184252f0415e92eecf6be3d0e
Lynn Knapp <ebonyeek@gmail.com>
2019-10-21 16:01:58 -0600

    animal form finished

--- client/src/components/AdoptionForm.js
+++ client/src/components/AdoptionForm.js
@@ -4,44 +4,63 @@ class AdoptionForm extends Component{
     constructor(){
         super()
         this.state={
+            name: "",
+            emailaddress: "",
+            usrtel: Number,
+            contactTime: ""
 
         }
+        
+    }
+    handleChange = e =>{
+        this.setState({
+        [e.target.name]: e.target.value
+        })
+    }
+    handleSubmit = e =>{
+        e.prevent.default()
+        const submits ={
+            name: this.state.name,
+            emailaddress: this.state.emailaddress,
+            usrtel: this.state.usrtel,
+            contactTime: this.state.contactTime
+        }
+        alert(submits)
     }
     render(){
         return(
             <div>
-            <h2>Please fill out this form if interested in adopting a pet.</h2>
-              <form>
-                <select>
-                    <option value="cat">Cat</option>
-                    <option value="dog">Dog</option>
-                </select>    
+            <h2>Please fill out this form if interested in adopting this pet.</h2>
+              <form onSubmit={this.handleSubmit}> 
                 <input
                 type="text" 
                 name="name" 
-                value={name} 
-                onChange={handleChange} 
-                placeholder="Name"/>
+                value={this.state.name} 
+                onChange={this.handleChange} 
+                placeholder="Name"/>Full Name
               <input
                 type="email"
                 name="emailaddress"
-                value={email}
-                onchange={handleChange}
-                placeholder="Email Address"/>
+                value={this.state.email}
+                onchange={this.handleChange}
+                placeholder="Email Address"/>Email Address
              <input
                 type="tel" 
                 name="usrtel"
-                value={phone}
-                onChange={handleChange}
+                value={this.state.usrtel}
+                onChange={this.handleChange}
                 placeholder="Contact Number"/>
-             <input
-                type="text"
-                name="contact-time"
-                value={contact-time}
-                onchange={handleChange} 
-                placeholder="Best time to contact" />
-
-                
+            <select>
+                <option placeholder="selectOne">Select pet to adopt</option>
+                <option value="cat">Cat</option>
+                <option value="dog">Dog</option>
+            </select>       
+            <select>
+                <option placeholder="Best time to contact">Best time to contact</option>
+                <option value="morning">Morning</option>
+                <option value="afternoon">Afternoon</option>
+            </select>
+            <button onchange={this.handleSubmit}>Submit</button>       
             </form> 
             </div>
         )
--- client/src/components/AnimalDetails.js
+++ client/src/components/AnimalDetails.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import AdoptionForm from './AdoptionForm.js'
 
 const AnimalDetails = (props) => {
     
