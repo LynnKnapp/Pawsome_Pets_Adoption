557b528e601f9aeddae2b0c0888ddebffc1422cd
Lynn Knapp <ebonyeek@gmail.com>
2019-10-21 16:34:59 -0600

    about us

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -3,7 +3,9 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            About
+            <h1>About Us</h1>
+            <h3>Our Mission Statement</h3>
+            <p>To provide no-kill solutions to reduce the number of homeless cats and dogs in Salt Lake County through education, rehabilitation, and re-homing with the support and cooperation of our community.</p>
         </div>
     )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,6 +5,8 @@ import Carousel from './Carousel'
 const Home = () => {
     return (
         <div className="homeMain">
+        <h3>Our Mission Statement</h3>
+        <p>To provide no-kill solutions to reduce the number of homeless cats and dogs in Salt Lake County through education, rehabilitation, and re-homing with the support and cooperation of our community.</p>
             <Carousel />
 
             <section className="whoWeAre">
