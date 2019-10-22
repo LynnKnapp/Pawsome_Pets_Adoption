843eb0a9d8151e98b3f05e7dd1b7660cec652cf1
Caetlyn Venche <caetlynvenche@gmail.com>
2019-10-22 15:13:01 -0600

    style forms, and recently adopted.

--- client/src/components/AdoptedPets.js
+++ client/src/components/AdoptedPets.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 const AdoptedPets = () => {
     return (
-        <>
+        <div className="adoptedPets">
             <h2>They Found Their Forever Home!!</h2>
         <div className="adopt">
             <section className="adoptDog">
-                <h1>Dogs</h1>
+                <h3>Dogs</h3>
                 <div>
                     <img src="https://images.pexels.com/photos/2055231/pexels-photo-2055231.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="Shih Tzu"/>
+                    <div>
+                        <p>Words belong here</p>
+                    </div>
+                    
                 </div>
                 <div>
                     <img src="https://images.pexels.com/photos/341378/pexels-photo-341378.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="Camping Family" />
@@ -19,7 +23,7 @@ const AdoptedPets = () => {
             </section>
 
             <section className="adoptCat">
-                <h1>Cats</h1>
+                <h3>Cats</h3>
                 <div>
                     <img src="https://images.pexels.com/photos/1838549/pexels-photo-1838549.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="Lounging Cat"/>
                 </div>
@@ -31,7 +35,7 @@ const AdoptedPets = () => {
                 </div>
             </section>
         </div>
-        </>
+        </div>
     )
 }
 
