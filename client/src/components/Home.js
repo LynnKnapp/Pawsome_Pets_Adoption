938a717393cbf4e1f0de1e41a110877f549727e8
Caetlyn Venche <caetlynvenche@gmail.com>
2019-10-22 10:17:42 -0600

    home and canvas html change and format

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,8 +3,11 @@ import Carousel from './Carousel'
 const Home = () => {
     return (
         <div className="homeMain">
-        <h3>Our Mission Statement</h3>
-        <p>To provide no-kill solutions to reduce the number of homeless cats and dogs in Salt Lake County through education, rehabilitation, and re-homing with the support and cooperation of our community.</p>
+            <section className="mission">
+                <h3>Our Mission Statement</h3>
+                <p>To provide no-kill solutions to reduce the number of homeless cats and dogs in Salt Lake County through education, rehabilitation, and re-homing with the support and cooperation of our community.</p>
+            </section>
+        
             <Carousel />
             <section className="blog">
                 <h2>Sweetened with Vanilla</h2>
@@ -17,33 +20,6 @@ const Home = () => {
                 </p> 
                 <h3>-Claire Ebbers & Vanilla</h3>
             </section>
-            <section className="whoWeAre">
-                <h2>Who We Are</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde assumenda at tempora, asperiores fugit perspiciatis officiis soluta, nisi ducimus et hic ad, enim sequi ut harum est repudiandae. Iure, ea.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aperiam ducimus esse odio animi voluptate dignissimos dicta sint dolore ipsam eveniet officiis, eaque, cupiditate obcaecati deleniti modi, reprehenderit consequuntur vero.</p>
-            </section>
-            <div className="testimony">
-                <section>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odio reiciendis et magnam consequuntur quo.</p>
-                    <h3>-Name</h3>
-                </section>
-                <section></section>
-                <section></section>
-                <section>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odio reiciendis et magnam consequuntur quo.</p>
-                    <h3>-Name</h3>
-                </section>
-                <section>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odio reiciendis et magnam consequuntur quo.</p>
-                    <h3>-Name</h3>
-                </section>
-                <section></section>
-                <section></section>
-                <section>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odio reiciendis et magnam consequuntur quo.</p>
-                    <h3>-Name</h3>
-                </section>
-            </div>
         </div>
     )
 }
