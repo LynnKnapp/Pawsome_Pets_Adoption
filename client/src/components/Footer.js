3c9a527199c85f44137772b449e0bb71b33fb2f7
Caetlyn Venche <caetlynvenche@gmail.com>
2019-10-23 12:11:59 -0600

    footer styling with media

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,13 +2,18 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
-            <h1>Pawesome Pets</h1>
-            <h3>1313 Mockingbird Ln</h3>
-            <h3>Salt Lake City, Utah</h3>
-            <h4>pawesome.pets@gmail.com</h4>
-            <h1>Life Saving Center Hours</h1>
-            <h3>Mon-Sat 11am-6pm</h3>
-            <h3>Sunday 12pm-4pm</h3>     
+            <div>
+                <h1>Pawesome Pets</h1>
+                <p>1313 Mockingbird Ln</p>
+                <p>Salt Lake City, Utah</p>
+                <p>pawesome.pets@gmail.com</p>
+            </div>
+
+            <div>
+                <h1>Life Saving Center Hours</h1>
+                <p>Mon-Sat 11am-6pm</p>
+                <p>Sunday 12pm-4pm</p>
+            </div>
         </footer>
     )
 }
