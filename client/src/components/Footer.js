d5e728416fdee63d87c7f4fab34b1abac7ef7ec1
Caetlyn Venche <caetlynvenche@gmail.com>
2019-10-23 11:39:07 -0600

    added footer and moved donate to right section of nav

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
 const Footer = () => {
     return (
         <footer>
-            footer
+            <h1>Pawesome Pets</h1>
+            <h3>1313 Mockingbird Ln</h3>
+            <h3>Salt Lake City, Utah</h3>
+            <h4>pawesome.pets@gmail.com</h4>
+            <h1>Life Saving Center Hours</h1>
+            <h3>Mon-Sat 11am-6pm</h3>
+            <h3>Sunday 12pm-4pm</h3>     
         </footer>
     )
 }
-
 export default Footer
