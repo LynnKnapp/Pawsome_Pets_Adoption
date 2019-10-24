a7a25977c000eadb9981e772c3557057ca792561
Lynn Knapp <ebonyeek@gmail.com>
2019-10-24 09:23:21 -0600

    final touches

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -8,16 +8,9 @@ const Nav = (props) => {
         <nav>   
             <Link className={props.location.pathname === "/" ? "active" : ""} to="/"><h1>P<i className="fas fa-paw fa-xs"></i>WESOME PETS</h1></Link>
             <div>   
-<<<<<<< HEAD
-                <Link to="/available">Adoption</Link>
-                <Link to="/search">Search Pets</Link>
-                <Link to="/adopt">Recently Adopted</Link>
-                <Link to="/about">About Us</Link>
-=======
                 <Link className={props.location.pathname === "/available" ? "active" : ""} to="/available">Adoption</Link>
                 <Link className={props.location.pathname === "/adopt" ? "active" : ""} to="/adopt">Recently Adopted</Link>
                 <Link className={props.location.pathname === "/about" ? "active" : ""} to="/about">About Us</Link>
->>>>>>> 44c7c852d580c83c8f716fb8ff80670b39511976
             </div>
             <div className="donate">
                 <Link to="/donate" className="donateBtn">Donate</Link>
