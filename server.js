955588a9a7c3e01ff29323f2308b0c2bb846bf10
Randon Shaw <randon.shaw@gmail.com>
2019-10-25 22:36:48 -0600

    first deploy attempt

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+require('dotenv').config()
 const express =require('express')
 const app =express()
 const morgan =require('morgan')
 const mongoose =require('mongoose')
 const PORT =process.env.PORT || 6004
+const path = require("path")
+
 
 app.use(express.json())
 app.use(morgan('dev'))
-
-mongoose.connect('mongodb://localhost: 27017/pawsomedb',
+// console.log(process.env.PASSWORD)
+mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0-ibfpt.mongodb.net/petAdoption?retryWrites=true&w=majority`
+,
     {
         useNewUrlParser: true,
         useFindAndModify: true,
@@ -25,6 +29,10 @@ app.use((err,req,res,next)=>{
     return res.send({errMsg: err.message})
 })
 
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () =>{
     console.log(6004)
