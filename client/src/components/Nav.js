353daca96490399bc7e7afcb9d61084cd6defeb1
Lynn Knapp <ebonyeek@gmail.com>
2019-11-22 15:42:10 -0500

    convert sass to css

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
+import '../styles/navbar.css'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faPaw } from '@fortawesome/free-solid-svg-icons'''
 
 const Nav = (props) => {
     return (
-        <nav>   
-            <Link className={props.location.pathname === "/" ? "active" : ""} to="/"><h1>P<i className="fas fa-paw fa-xs"></i>WESOME PETS</h1></Link>
-            <div>   
-                <Link className={props.location.pathname === "/available" ? "active" : ""} to="/available">Adoption</Link>
-                <Link className={props.location.pathname === "/search" ? "active" : ""} to="/search">Search Pets</Link>
-                <Link className={props.location.pathname === "/adopt" ? "active" : ""} to="/adopt">Recently Adopted</Link>
-                <Link className={props.location.pathname === "/about" ? "active" : ""} to="/about">About Us</Link>
+        <div className='navbar'> 
+            <div className='header'>  
+                <Link className={props.location.pathname === "/" ? "active" : ""} to="/"><h1>P<i className="fas fa-paw fa-xs"></i>WESOME PETS</h1></Link>
             </div>
-
-            <div className="donate">
-                <Link to="/donate" className="donateBtn">Donate</Link>
+            <div className='links'>
+                <Link className={props.location.pathname === "/available" ? "active" : ""} to="/available">Available Pets</Link>
+                <Link className={props.location.pathname === "/search" ? "active" : ""} to="/search">Search Pets </Link>
+                <Link className={props.location.pathname === "/adopt" ? "active" : ""} to="/adopt">Recently Adopted|</Link>
+                <Link className={props.location.pathname === "/about" ? "active" : ""} to="/about">About Us </Link>
+            </div> 
+            <div className='donate'>
+                <Link to="/donate" className="donateBtn">Donate</Link> 
             </div>
-        </nav>
+        </div>
     )
 }
 
