a7664b9012252d2fdb264d1aa011ab0e7d7e28d1
Lynn Knapp <ebonyeek@gmail.com>
2019-11-26 14:24:32 -0500

    removed size from dog

--- client/src/components/Dog.js
+++ client/src/components/Dog.js
@@ -9,8 +9,7 @@ const Dog = (props) => {
                 <div className= 'petInfo'>
                     <h3>Name: {props.name}</h3>
                     <h5>Breed: {props.breed}</h5>
-                    <h5>Gender: {props.gender}</h5>
-                    <h5>Size: {props.size}</h5>
+                    <h5>Gender: {props.gender}</h
                     <h5>Age: {props.age}</h5>
                     {props.shed ? <h5>Sheds: Yes</h5> : <h5>Sheds: No</h5>}
                     {props.goodWithKids ? <h5>Good with Kids: Yes!</h5> : <h5> Good with Kids: Unfortunately, No</h5>}
